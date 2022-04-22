732cd22e11cd0dcafd12ed5ed6e57ccf10553514
sito8943 <sito8943@gmail.com>
2022-04-22 17:42:49 -0400

    app

--- src/views/News/News.jsx
+++ src/views/News/News.jsx
@@ -4,15 +4,34 @@ import { useEffect } from "react";
 // @mui components
 import { Box } from "@mui/material";
 
-// layouts
-import Hero from "layouts/Hero/Hero";
+// contexts
+import { useRoute } from "context/RouterProvider";
+
+// own components
+import GridItem from "components/GridItem/GridItem";
+import ItemGrid from "components/ItemGrid/ItemGrid";
+import Search from "./Search/Search";
 
 const News = () => {
-  useEffect(() => {}, []);
+  const { setRouteState } = useRoute();
+
+  const items = [
+    <ItemGrid />,
+    <ItemGrid />,
+    <ItemGrid />,
+    <ItemGrid />,
+    <ItemGrid />,
+    <ItemGrid />,
+  ];
+
+  useEffect(() => {
+    setRouteState({ type: "set", to: 4 });
+  }, []);
 
   return (
     <Box>
-      <Hero />
+      <Search />
+      <GridItem content={items} />
     </Box>
   );
 };
